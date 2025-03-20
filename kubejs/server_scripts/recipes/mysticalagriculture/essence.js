7d6f67524f1fc0c7a7d8e6b30952446fb8f7582d
Quaky <info@quaky.cz>
2025-03-20 08:46:12 +0100

    Fix music discs, gunpowder, rotten flesh

--- kubejs/server_scripts/recipes/mysticalagriculture/essence.js
+++ kubejs/server_scripts/recipes/mysticalagriculture/essence.js
@@ -42,6 +42,33 @@ ServerEvents.recipes(event => {
     )
   }
 
+  function basic_2x2(inputTL, inputTR, inputBL, inputBR, output) {
+    event.shaped(
+      output,
+      [
+        "AB ",
+        "CD ",
+        "   "
+      ],
+      {
+        A: inputTL,
+        B: inputTR,
+        C: inputBL,
+        D: inputBR
+      }
+    )
+  }
+
+  function music_disc(dye, disc) {
+    basic_2x2(
+      "mysticalagriculture:blank_record",
+      "mysticalagriculture:creeper_essence",
+      "mysticalagriculture:skeleton_essence",
+      dye, 
+      disc
+    )
+  }
+
   // Add uses for elemental essences
   checked_2x2('mysticalagriculture:air_essence', 'mysticalagriculture:fire_essence', '12x minecraft:sand')
   checked_2x2('mysticalagriculture:earth_essence', 'mysticalagriculture:fire_essence', '16x minecraft:cobblestone')
@@ -85,4 +112,60 @@ ServerEvents.recipes(event => {
   // fix crafting arrows from skeleton essence
   event.remove({ id: "mysticalagriculture:essence/minecraft/arrow"})
   row("mysticalagriculture:skeleton_essence", "8x minecraft:arrow")
+
+  // fix crafting rotten fles from zombie essence
+  event.remove({ id: "mysticalagriculture:essence/minecraft/rotten_flesh"})
+  row("mysticalagriculture:zombie_essence", "12x minecraft:rotten_flesh")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/slime_ball"})
+  row("mysticalagriculture:slime_essence", "8x minecraft:slime_ball")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/gunpowder"})
+  row("mysticalagriculture:creeper_essence", "6x minecraft:gunpowder")
+
+  // music discs
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_13"})
+  music_disc("#forge:dyes/yellow", "minecraft:music_disc_13")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_cat"})
+  music_disc("minecraft:lime_dye", "minecraft:music_disc_cat")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_blocks"})
+  music_disc("minecraft:orange_dye", "minecraft:music_disc_blocks")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_chirp"})
+  music_disc("minecraft:red_dye", "minecraft:music_disc_chirp")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_far"})
+  music_disc("minecraft:cyan_dye", "minecraft:music_disc_far")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_mall"})
+  music_disc("minecraft:purple_dye", "minecraft:music_disc_mall")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_mellohi"})
+  music_disc("minecraft:magenta_dye", "minecraft:music_disc_mellohi")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_stal"})
+  music_disc("minecraft:black_dye", "minecraft:music_disc_stal")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_strad"})
+  music_disc("minecraft:white_dye", "minecraft:music_disc_strad")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_ward"})
+  music_disc("minecraft:green_dye", "minecraft:music_disc_ward")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_11"})
+  music_disc("minecraft:gray_dye", "minecraft:music_disc_11")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_wait"})
+  music_disc("minecraft:light_blue_dye", "minecraft:music_disc_wait")
+
+  event.remove({ id: "mysticalagriculture:essence/minecraft/music_disc_pigstep"})
+  basic_2x2(
+    "mysticalagriculture:blank_record",
+    "mysticalagriculture:gold_essence", 
+    "mysticalagriculture:nether_essence",
+    "mysticalagriculture:netherite_essence",
+    "minecraft:music_disc_pigstep"
+  )
 })
